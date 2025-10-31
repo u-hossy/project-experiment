da2e09d15d6333bff569359cb68529e8f802077b
u-hossy <95125392+u-hossy@users.noreply.github.com>
2025-10-31 15:50:06 +0900

    frontend: 様々なPropsを受け取れるように拡張

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,18 @@
+import { cn } from "@/lib/utils";
 import { HeaderMenu } from "./HeaderMenu";
 
-export default function Header() {
+export default function Header({
+  className,
+  ...props
+}: React.ComponentProps<"header">) {
   return (
-    <header className="fixed top-0 left-0 z-50 w-full border-b bg-background shadow-md">
+    <header
+      className={cn(
+        "fixed top-0 left-0 z-50 w-full border-b bg-background shadow-md",
+        className,
+      )}
+      {...props}
+    >
       <div className="flex h-16 w-full items-center justify-between px-4">
         <div className="flex items-center">
           <img
