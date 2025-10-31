815ed776acd52c537f9355d214b3df169a7a4fc4
masa-22 <183780928+masa-22@users.noreply.github.com>
2025-10-31 17:33:45 +0900

    feature/add-result

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import BillingTabList from "./components/BillingTabList";
 import Header from "./components/Header";
 import MemberList from "./components/MemberList";
 import PaymentNetwork from "./components/NetworkGraph";
+import Result from "./components/ExampleResultTab";
 import { datas } from "./tmp/tmp_datas";
 
 function App() {
