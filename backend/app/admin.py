2ad38a0267454051f15f78ed99fb7aa15b0f1e27
fuyuUSG <234328264+fuyuUSG@users.noreply.github.com>
2025-11-21 00:02:31 +0900

    backend:データベース作成のためmodelsとadminを変更

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Events, Members, Payments, Results
+admin.site.register(Events)
+admin.site.register(Members)
+admin.site.register(Payments)
+admin.site.register(Results)
