a780804d159619357a01691a7c4e934a1519ca10
fic <207446482+tyyttty@users.noreply.github.com>
2025-12-03 21:25:26 +0900

    frontend:App.tsxのアルゴリズムと結果のページのpath名の変更

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import EventRoot from "./components/EventRoot";
 import Layout from "./components/Layout";
-import ResultPage from "./pages/AlgorithmAndResultPage";
 import TopPage from "./pages/TopPage";
 import type { Member } from "./types/member";
 import type { Payment } from "./types/payment";
-import BillingWrapper from "./websocket/BillingWrapper";
 import MembersWrapper from "./websocket/MembersWrapper";
+import BillingWrapper from "./websocket/BillingWrapper";
+import AlgorithmAndResultPage from "./pages/AlgorithmAndResultPage";
 
 function App() {
   const [members, setMembers] = useState<Member[]>([]);
@@ -20,32 +20,32 @@ function App() {
           <Route index element={<Navigate to="/top" replace />} />
 
           <Route path="top" element={<TopPage />} />
-          <Route path=":eventId" element={<EventRoot />}>
-            <Route
-              path=":members"
-              element={
-                <MembersWrapper
-                  members={members}
-                  setMembers={setMembers}
-                  setPayments={setPayments}
-                />
-              }
-            />
-            <Route
-              path="billing"
-              element={
-                <BillingWrapper
-                  members={members}
-                  payments={payments}
-                  setMembers={setMembers}
-                  setPayments={setPayments}
-                />
-              }
-            />
-            <Route
-              path="algorithmAndresults"
-              element={<ResultPage members={members} payments={payments} />}
-            />
+          <Route path=":eventId" element={<EventRoot/>}>
+          <Route
+            path=":members"
+            element={
+              <MembersWrapper
+                members={members}
+                setMembers={setMembers}
+                setPayments={setPayments}
+               />
+            }
+          />
+          <Route
+            path="billing"
+            element={
+              <BillingWrapper
+                members={members}
+                payments={payments}
+                setMembers={sestMembers}
+                setPayments={setPayments}
+              />
+            }
+          />
+          <Route
+            path=":algorithmAndresults"
+            element={<AlgorithmAndResultPage members={members} payments={payments} />}
+          />
           </Route>
         </Route>
       </Routes>
